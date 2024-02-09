c11d75d8e2bc0b38b4f4e1068cbf93b5549a2f2e
AsiiaPine <asiiapine@gmail.com>
2024-02-09 16:49:46 +0300

    fix int_led pins

--- Core/Src/stm32f1xx_hal_msp.c
+++ Core/Src/stm32f1xx_hal_msp.c
@@ -358,10 +358,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     /**TIM1 GPIO Configuration
     PA8     ------> TIM1_CH1
     */
-    GPIO_InitStruct.Pin = INT_RGB_LED_RED_Pin;
+    GPIO_InitStruct.Pin = INT_RGB_LED_GREEN_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(INT_RGB_LED_RED_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(INT_RGB_LED_GREEN_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN TIM1_MspPostInit 1 */
 
@@ -377,10 +377,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     /**TIM2 GPIO Configuration
     PA15     ------> TIM2_CH1
     */
-    GPIO_InitStruct.Pin = INT_RGB_LED_GREEN_Pin;
+    GPIO_InitStruct.Pin = INT_RGB_LED_RED_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(INT_RGB_LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(INT_RGB_LED_RED_GPIO_Port, &GPIO_InitStruct);
 
     __HAL_AFIO_REMAP_TIM2_PARTIAL_1();
 
@@ -400,7 +400,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     PB4     ------> TIM3_CH1
     PB5     ------> TIM3_CH2
     */
-    GPIO_InitStruct.Pin = EXT_RGB_LED_BLUE_Pin|EXT_RGB_LED_GREEN_Pin|EXT_RGB_LED_RED_Pin;
+    GPIO_InitStruct.Pin = EXT_RGB_LED_BLUE_Pin|EXT_RGB_LED_RED_Pin|EXT_RGB_LED_GREEN_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -444,7 +444,7 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
     PA15     ------> TIM2_CH1
     PB3     ------> TIM2_CH2
     */
-    HAL_GPIO_DeInit(INT_RGB_LED_GREEN_GPIO_Port, INT_RGB_LED_GREEN_Pin);
+    HAL_GPIO_DeInit(INT_RGB_LED_RED_GPIO_Port, INT_RGB_LED_RED_Pin);
 
     HAL_GPIO_DeInit(PWM5_GPIO_Port, PWM5_Pin);
 
